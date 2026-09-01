480b916c07325f6a4bc5155d6db8a9b60098930f
simranbist62 <seemranbist@gmail.com>
2026-09-01 10:03:22 +0545

    Add get current user and logout feature

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,6 @@
 const API_URL = "https://crm-backend-eh94.onrender.com/api";
 
-export async function loginUser(
-  email: string,
-  password: string
-) {
+export async function loginUser(email: string, password: string) {
   const response = await fetch(`${API_URL}/auth/login`, {
     method: "POST",
     headers: {
@@ -29,7 +26,7 @@ export async function registerUser(
   email: string,
   password: string,
   role: string,
-  designation: string
+  designation: string,
 ) {
   const response = await fetch(`${API_URL}/auth/register`, {
     method: "POST",
@@ -52,4 +49,42 @@ export async function registerUser(
   }
 
   return data;
+}
+
+//Get current user
+export async function getCurrentUser() {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    throw new Error("No authentication token found");
+  }
+  const response = await fetch(`${API_URL}/auth/me`, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || "Failed to get current user");
+  }
+  return data;
+}
+
+//Logout
+export async function logoutUser() {
+  const token = localStorage.getItem("token");
+  try {
+    const response = await fetch(`${API_URL}/auth/logout`, {
+      method: "POST",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.message || "Logout failed");
+    }
+    return data;
+  } finally {
+    // remove local authentication data
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  }
 }
